f4e3a0de28174c17481eb04eab619be50515a1a1
mayane_cesario <mayane_cesario@10.1.48.184>
2014-05-27 14:34:10 -0300

    Inicialização de um projeto.

---
+++ src/teste/Teste.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mayane_cesario
+ */
+public class Teste {
+
+   
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Testando");
+    }
+}
